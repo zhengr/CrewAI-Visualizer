b8dcafc2ca9b3c3a4764d66cc330e5db7bac90e6
Robin Zheng <zhengr@msn.com>
2024-03-26 20:17:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Prisma Migrations
+echo "Applying Prisma Migrations..."
+npx prisma generate
+npx prisma migrate deploy
+
+# Building the project
+echo "Building the project..."
+npm run build
+
+# Building is finished
+echo "Building is finished"
+
+# Run the project
+echo "Starting development server..."
 npm run dev
